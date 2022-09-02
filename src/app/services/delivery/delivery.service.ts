29edd60f45881aec4361ab41b2ef217c5a30fba5
EliezerJD <eliezerjerez1@gmail.com>
2022-09-02 14:41:41 -0500

    token service added

--- src/app/services/delivery/delivery.service.ts
+++ src/app/services/delivery/delivery.service.ts
@@ -1,30 +1,28 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient} from '@angular/common/http';
+import { TokenService } from '../token/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeliveryService {
-  data:any = [];
-  constructor(private http: HttpClient) {
-    this.data = JSON.parse(sessionStorage.getItem(environment.USER_SECRET_KEY) || '{}');
-  }
+  constructor(private http: HttpClient, private token: TokenService) {}
 
   getSavingsAccounts(){
-    return this.http.get(environment.ACCOUNTS_URL + this.data.idToken);
+    return this.http.get(environment.ACCOUNTS_URL + this.token.getToken());
   }
 
   getRecords(){
-    return this.http.get(environment.RECORDS_URL + this.data.idToken);
+    return this.http.get(environment.RECORDS_URL + this.token.getToken());
   }
 
   addRecord(data:any){
-    return this.http.post(environment.RECORDS_URL + this.data.idToken, {usuario:data.usuario, numeroCuenta:data.numeroCuenta, monto:data.monto, terminal: data.terminal, tipo:data.tipo, fechaUltimaAct:data.fechaUltimaAct});
+    return this.http.post(environment.RECORDS_URL + this.token.getToken(), {usuario:data.usuario, numeroCuenta:data.numeroCuenta, monto:data.monto, terminal: data.terminal, tipo:data.tipo, fechaUltimaAct:data.fechaUltimaAct});
   }
 
   addSavingAccount(data:any){
-    return this.http.post(environment.ACCOUNTS_URL + this.data.idToken, {idCliente:data.idCliente, numeroCuenta:data.numeroCuenta, saldo:data.saldo, estado: data.estado, fechaUltimaAct:data.fechaUltimaAct});
+    return this.http.post(environment.ACCOUNTS_URL + this.token.getToken(), {idCliente:data.idCliente, numeroCuenta:data.numeroCuenta, saldo:data.saldo, estado: data.estado, fechaUltimaAct:data.fechaUltimaAct});
   }
 
   getClients(){
